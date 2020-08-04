3f5cdc9331dccab38a065b5e1141ec4497005e31
NeoTRAN <neo_tran@outlook.com>
2020-08-04 09:50:54 -0500

    Styles images and img buttons

--- src/App.svelte
+++ src/App.svelte
@@ -59,6 +59,11 @@
 		saveTask = true;
 	}
 
+	const clearInput = () => {
+		task = {content: ''}
+		saveTask = true;
+	}
+
 	const handleSubmit = () => {
 		if (saveTask) addTask();
 		else updateTask();
@@ -93,9 +98,20 @@
 						<input required class="form-control" bind:value={task.content} type="text" placeholder="Write a new task">
 					</div>
 					<div class="col-2">
-						<button class="btn btn-info">
-							{#if saveTask}Agregar {:else} Actualizar {/if}
+						<button class="btn btn-light">
+							{#if saveTask} 
+								<img src="./img/add.png" class="imgRequired" alt=""> 
+							{:else} 
+								<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-check-all" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+									<path fill-rule="evenodd" d="M8.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14l.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z"/>
+							  	</svg> 
+							{/if}
 						</button>
+						{#if !saveTask} 
+								<button class="btn btn-light" on:click={clearInput}>
+									<img src="./img/close.png" class="imgRequired" alt="">
+								</button>
+						{/if}
 					</div>
 				</div>
 			</form>
@@ -104,10 +120,8 @@
 			<div class="row">
 				<div class="col-10">ToDo-List for Iron Bit by Ricoy</div>
 				<div class="col-2">
-					<button class="btn btn-dark"  on:click={deleteAllTask}>
-						<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-							<path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
-						</svg>
+					<button class="btn btn-ligth"  on:click={deleteAllTask}>
+						<img src="./img/reBin.png" class="imgRequired" alt="">
 					</button>
 				</div>
 			</div>
@@ -117,18 +131,14 @@
 				<div class="row">
 					<div class="col-10" id="contentTask">{task.content}</div>
 					<div class="col-2">
-						<button class="btn btn-primary" on:click={editTask(task)}>
-							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<button class="btn btn-ligth" on:click={editTask(task)}>
+							<svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 								<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
 								<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
 							</svg>
 						</button>
-						<button class="btn btn-danger" on:click={deleteTask(task.id)}>
-							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-								<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-								<path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
-								<path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
-							</svg>
+						<button class="btn btn-light" on:click={deleteTask(task.id)}>
+							<img src="./img/close.png" class="imgRequired" alt="">
 						</button>
 					</div>
 				</div>
